a1a306462ed228d5bd6ff3308851469af9378461
yselkowitz <yselkowitz@e127d73d-b18e-4ae1-a744-a22e6a3ccd30>
2010-04-15 09:40:16 +0000

    java/jamvm: fix alternatives postinstall
    git-svn-id: https://cygwin-ports.svn.sourceforge.net/svnroot/cygwin-ports/ports/trunk@9088 e127d73d-b18e-4ae1-a744-a22e6a3ccd30

--- postinstall.sh
+++ postinstall.sh
@@ -2,4 +2,4 @@ rank=150
 
 /usr/sbin/update-alternatives \
 	--install "/usr/bin/java" "java" "/usr/bin/jamvm.exe" $rank \
-	--slave "/usr/lib/tools.jar" "tools.jar" "/usr/share/classpath/tools.jar"
+	--slave "/usr/lib/tools.jar" "tools.jar" "/usr/share/classpath/tools.zip"
